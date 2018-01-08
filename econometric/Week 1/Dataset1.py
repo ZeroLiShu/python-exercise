37458ea8939c775010ddca51dc9bd20f0f2b5eb9
ZeroLiShu <lishufox@icloud.com>
2018-01-08 22:05:19 +0800

    add dataset1 avarage png

--- econometric/Week 1/Dataset1.py
+++ econometric/Week 1/Dataset1.py
@@ -10,3 +10,9 @@ df = pd.read_csv('Dataset1.txt', sep='\t')
 del df['Observ.']
 plt = df.plot(x='Price', y='Sales', kind='scatter').get_figure()
 plt.savefig('Dataset1_scatter.png')
+
+#Group by Price, Avarage for Sales
+grouped = df.groupby('Price')
+df2 = grouped.mean()
+plt = df2.plot().get_figure()
+plt.savefig('Dataset1_avarage.png')
