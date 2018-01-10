18e9c9f2aae155ce92cc3b9b22b66ef891b18531
ZeroLiShu <lishufox@icloud.com>
2018-01-10 21:14:38 +0800

    add linear regression prediction

--- econometric/Week 1/Dataset1.py
+++ econometric/Week 1/Dataset1.py
@@ -4,15 +4,27 @@
 import matplotlib as mp
 mp.use('Agg')
 import pandas as pd
+from sklearn import linear_model
+import matplotlib.pyplot as plt
 
 #Price to Sales
 df = pd.read_csv('Dataset1.txt', sep='\t')
 del df['Observ.']
-plt = df.plot(x='Price', y='Sales', kind='scatter').get_figure()
+
+regr = linear_model.LinearRegression()
+regr.fit(df['Price'].values.reshape(-1, 1), df['Sales'])
+b, a = regr.coef_, regr.intercept_
+
+#Price = 50, 58
+print('Price = 50, Sales = ', a + b * 50)
+print('Price = 58, Sales = ', a + b * 58)
+
+plt.scatter(df['Price'], df['Sales'], color='blue')
+plt.plot(df['Price'], regr.predict(df['Price'].values.reshape(-1, 1)), color='red')
 plt.savefig('Dataset1_scatter.png')
 
 #Group by Price, Avarage for Sales
 grouped = df.groupby('Price')
 df2 = grouped.mean()
-plt = df2.plot().get_figure()
-plt.savefig('Dataset1_avarage.png')
+plt2 = df2.plot().get_figure()
+plt2.savefig('Dataset1_avarage.png')
