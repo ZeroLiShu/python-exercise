9267c55a27d78c5f35ca397038ba544be58e9925
ZeroLiShu <lishufox@icloud.com>
2018-01-31 22:10:54 +0800

    add ExerHelper.py

---
+++ econometric/Week 1/ExerHelper.py
@@ -0,0 +1,46 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+import math
+
+class ExerRegression:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def fit(self):
+        #Compute mean
+        self.x_mean = self.x.mean()
+        self.y_mean = self.y.mean()
+
+        #Compute x, y diff mean
+        self.x_diff = (self.x - self.x_mean)
+        self.y_diff = (self.y - self.y_mean)
+
+        #Compute b
+        self.b = (self.x_diff * self.y_diff).sum() / (self.x_diff**2).sum()
+        self.a = self.y_mean - self.b * self.x_mean
+
+        return self.a, self.b
+
+    def residual(self):
+        self.e = self.y - self.a - self.x * self.b
+
+        return self.e
+
+    def R_square(self):
+        self.residual()
+        self.R_2 = 1 - (self.e**2).sum() / (self.y_diff**2).sum()
+
+        return self.R_2
+
+    def s_square(self):
+        self.residual()
+        self.s_2 = ((self.e - self.e.mean())**2).sum() / (len(self.e) - 2)
+
+        return self.s_2
+
+    def SEb(self):
+        self.SEb = math.sqrt(self.s_2 / (self.x_diff**2).sum())
+
+        return self.SEb
--- econometric/Week 1/TrainExer13.py
+++ econometric/Week 1/TrainExer13.py
@@ -1,41 +1,31 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import matplotlib as mp
 mp.use('Agg')
 import pandas as pd
 import math
+from ExerHelper import ExerRegression
 
 df = pd.read_csv('TrainExer13.txt', sep='\t')
 
-#Compute mean, x=Game, y=Time
-x_mean = df['Game'].mean()
-y_mean = df['Winning time men'].mean()
-
-#Compute x, y diff mean
-x_diff = (df['Game'] - x_mean)
-y_diff = (df['Winning time men'] - y_mean)
-
-#Compute b
-b = (x_diff * y_diff).sum() / (x_diff**2).sum()
-a = y_mean - b * x_mean
+ER = ExerRegression(df['Game'], df['Winning time men'])
+a, b = ER.fit()
 
 print('a = ', a, ' b= ', b)
 
 #R Square
-e = df['Winning time men'] - a - df['Game'] * b
-e_square = e**2
-R_square = 1 - e_square.sum() / (y_diff**2).sum()
+R_square = ER.R_square()
 
 print('R_square = ', R_square)
 
 #s Square
-s_square = ((e - e.mean())**2).sum() / (len(e) - 2)
+s_square = ER.s_square()
 
 print('s_square = ', s_square)
 
 #SEb
-SEb = math.sqrt(s_square / (x_diff**2).sum())
+SEb = ER.SEb()
 
 print('SEb = ', SEb)
 
