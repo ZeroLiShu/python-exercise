acbbe91f566596e08f8100e5c79a724cb4c771c0
ZeroLiShu <lishufox@icloud.com>
2019-06-23 14:54:48 +0800

    fix dimension problems

--- statistic-learning-methods/perceptron.py
+++ statistic-learning-methods/perceptron.py
@@ -1,6 +1,9 @@
-import numpy as np
+#!
 
-def f(w, x, b):
+import numpy as np
+import pysnooper
+
+def predict(w, x, b):
     """
     This function is the model of perceptron which is a linear classification model.
 
@@ -11,6 +14,7 @@ def f(w, x, b):
     """
     return np.sign(np.dot(w, x) + b)
 
+@pysnooper.snoop()
 def train(x, y, h):
     """
     This function uses training set {x, y} to calcualte w and b until there's no misclassification points
@@ -20,9 +24,22 @@ def train(x, y, h):
         y, output space, y[i] in {-1, 1}
         h, learing rate, 0 < h <= 1 
     """
-    w = np.zeros(y.shape)
+    [rows, cols] = x.shape
+    w = np.zeros((rows, 1))
     b = 0
-    f = np.dot(w, x) + b
+    f = np.dot(w.T, x) + b
     L = y * f
     idx = np.where(L <= 0)
-    
+
+
+
+    #iterator
+
+if __name__ == "__main__":
+    #x' shape is 2 rows, 3 columns, n=2, N=3
+    x = np.array([[1,1,1], [2,2,2]])
+
+    #y'shape is 1 row, 3 columns, n=1, N=3
+    y = np.array([1,-1,-1])
+    h = 0.1
+    train(x, y, h)
